580fa387e2d76b82f16fe68d7fee477e627d9aaa
himawari <1156544355@qq.com>
2023-02-21 18:06:35 +0800

    🐛 go不可重入锁

--- webui.go
+++ webui.go
@@ -5,20 +5,15 @@ func (manager *Manager[CTX]) initUser() error {
 	return manager.D.Create("__user", &User{})
 }
 
-// CreateUser 创建用户
-func (manager *Manager[CTX]) CreateUser(u User) error {
-	manager.Lock()
-	defer manager.Unlock()
-	return manager.D.Insert("__user", &u)
-}
-
 // CreateOrUpdateUser 创建或修改用户密码
 func (manager *Manager[CTX]) CreateOrUpdateUser(u User) error {
 	manager.Lock()
 	defer manager.Unlock()
-	canFind, err := manager.CanFindUser(u)
+	var fu User
+	err := manager.D.Find("__user", &fu, "WHERE username = "+u.Username+"AND password = "+u.Password)
+	canFind := err == nil && fu.Username == u.Username
 	if err != nil || !canFind {
-		err = manager.CreateUser(u)
+		err = manager.D.Insert("__user", &u)
 		return err
 	}
 	err = manager.D.Del("__user", "WHERE username ="+u.Username)
