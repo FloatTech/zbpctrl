7a9ecab259d9e467be9bb880048d6c15cde9cb57
himawari <1156544355@qq.com>
2023-02-23 17:31:34 +0800

    🐛正确使用锁

--- webui.go
+++ webui.go
@@ -7,26 +7,33 @@ func (manager *Manager[CTX]) initUser() error {
 
 // CreateOrUpdateUser 创建或修改用户密码
 func (manager *Manager[CTX]) CreateOrUpdateUser(u User) error {
-	manager.Lock()
-	defer manager.Unlock()
+	manager.RLock()
 	var fu User
 	err := manager.D.Find("__user", &fu, "WHERE username = "+u.Username+"AND password = "+u.Password)
+	manager.RUnlock()
 	canFind := err == nil && fu.Username == u.Username
 	if err != nil || !canFind {
+		manager.Lock()
 		err = manager.D.Insert("__user", &u)
+		manager.Unlock()
 		return err
 	}
+	manager.Lock()
 	err = manager.D.Del("__user", "WHERE username ="+u.Username)
+	manager.Unlock()
 	if err != nil {
 		return err
 	}
-	return manager.D.Insert("__user", &u)
+	manager.Lock()
+	err = manager.D.Insert("__user", &u)
+	manager.Unlock()
+	return err
 }
 
 // FindUser 查找用户
 func (manager *Manager[CTX]) FindUser(u User) (fu User, err error) {
 	manager.RLock()
-	defer manager.RUnlock()
 	err = manager.D.Find("__user", &fu, "WHERE username = '"+u.Username+"' AND password = '"+u.Password+"'")
+	manager.RUnlock()
 	return
 }
